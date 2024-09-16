56b19565326d584992ee510f842ae761798cce76
pbartashov <p.bartashov@icloud.com>
2024-09-16 21:37:24 +0930

    Initial Commit

---
+++ CookBookSwiftUI/CookBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookSwiftUIApp.swift
+//  CookBookSwiftUI
+//
+//  Created by Pavel Bartashov on 16/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
